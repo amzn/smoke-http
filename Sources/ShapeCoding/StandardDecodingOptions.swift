c96ac3955663838627d079f94c609627c3f7640a
Simon Pilkington <simonpi@amazon.com>
2018-11-19 13:04:16 -0800

    Added documentation.

--- Sources/ShapeCoding/StandardDecodingOptions.swift
+++ Sources/ShapeCoding/StandardDecodingOptions.swift
@@ -29,6 +29,18 @@ public enum ShapeKeyDecodingStrategy {
     /// Dictionary entries are indicated based on the provided `ShapeMapDecodingStrategy`
     case useAsShapeSeparator(Character)
     
+    /// The decoder will spilt the shape keys on the expected name of an attribute to indicate a
+    /// nested structure that could include nested types, dictionaries and arrays.
+    ///
+    /// Array entries are indicated by a 1-based index
+    /// ie. ["theArray1": "Value1", "theArray2": "Value2] --> ShapeOutput(theArray: ["Value1", "Value2"])
+    /// Nested type attributes are indicated by the attribute keys
+    /// ie. ["theTypefoo": "Value1", "theTypebar": "Value2] --> ShapeOutput(theType: TheType(foo: "Value1", bar: "Value2"))
+    /// Dictionaries have dynamic membership based on what is present in the payload and therefore have no expected
+    /// attribute names. Dictionaries cannot use this strategy to indicate a nested structure beyond their own attributes.
+    /// ie. ["theMapfoo": "Value1", "theMapbar": "Value2] --> ShapeOutput(theMap: [foo: "Value1", bar: "Value2"])
+    /// The above transformation is possible with this strategy. This strategy cannot be used if the map's attributes
+    /// contains nested attributes. In these scenarios, a specific shape separator will need to be used.
     case useShapePrefix
     
     /// The decoder will decode shape keys into the attributes
