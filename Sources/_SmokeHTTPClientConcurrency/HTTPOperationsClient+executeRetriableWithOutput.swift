9ea92a193d04a9a22ef1f5ad1f801835e2dc537c
Simon Pilkington <simonpi@amazon.com>
2021-04-09 11:02:37 +1000

    Add initial async/await support.

---
+++ Sources/_SmokeHTTPClientConcurrency/HTTPOperationsClient+executeRetriableWithOutput.swift
@@ -0,0 +1,61 @@
+// Copyright 2018-2021 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License").
+// You may not use this file except in compliance with the License.
+// A copy of the License is located at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// or in the "license" file accompanying this file. This file is distributed
+// on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+// express or implied. See the License for the specific language governing
+// permissions and limitations under the License.
+//
+//  HTTPOperationsClient+executeRetriableWithOutput.swift
+//  _SmokeHTTPClientConcurrency
+//
+
+#if compiler(>=5.4) && $AsyncAwait
+
+import Foundation
+import NIO
+import NIOHTTP1
+import SmokeHTTPClient
+
+public extension HTTPOperationsClient {
+    
+    /**
+     Submits a request that will return a response body to this client asynchronously.
+
+     - Parameters:
+        - endpointPath: The endpoint path for this request.
+        - httpMethod: The http method to use for this request.
+        - input: the input body data to send with this request.
+        - invocationContext: context to use for this invocation.
+        - retryConfiguration: the retry configuration for this request.
+        - retryOnError: function that should return if the provided error is retryable.
+     - Returns: the response body.
+     - Throws: If an error occurred during the request.
+     */
+    func executeRetriableWithOutput<InputType, OutputType,
+            InvocationReportingType: HTTPClientInvocationReporting, HandlerDelegateType: HTTPClientInvocationDelegate>(
+        endpointOverride: URL? = nil,
+        endpointPath: String,
+        httpMethod: HTTPMethod,
+        input: InputType,
+        invocationContext: HTTPClientInvocationContext<InvocationReportingType, HandlerDelegateType>,
+        retryConfiguration: HTTPClientRetryConfiguration,
+        retryOnError: @escaping (HTTPClientError) -> Bool) async throws -> OutputType
+    where InputType: HTTPRequestInputProtocol, OutputType: HTTPResponseOutputProtocol {
+        return try await executeAsEventLoopFutureRetriableWithOutput(
+            endpointOverride: endpointOverride,
+            endpointPath: endpointPath,
+            httpMethod: httpMethod,
+            input: input,
+            invocationContext: invocationContext,
+            retryConfiguration: retryConfiguration,
+            retryOnError: retryOnError).get()
+    }
+}
+
+#endif
---
+++ Sources/_SmokeHTTPClientConcurrency/HTTPOperationsClient+executeWithoutOutput.swift
@@ -0,0 +1,77 @@
+// Copyright 2018-2021 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License").
+// You may not use this file except in compliance with the License.
+// A copy of the License is located at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// or in the "license" file accompanying this file. This file is distributed
+// on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+// express or implied. See the License for the specific language governing
+// permissions and limitations under the License.
+//
+//  HTTPOperationsClient+executeWithoutOutput.swift
+//  _SmokeHTTPClientConcurrency
+//
+
+#if compiler(>=5.4) && $AsyncAwait
+
+import Foundation
+import NIO
+import NIOHTTP1
+import SmokeHTTPClient
+import _Concurrency
+
+// TODO: Remove this when provided by swift-nio itself
+extension EventLoopFuture {
+    /// Get the value/error from an `EventLoopFuture` in an `async` context.
+    ///
+    /// This function can be used to bridge an `EventLoopFuture` into the `async` world. Ie. if you're in an `async`
+    /// function and want to get the result of this future.
+    public func get() async throws -> Value {
+        return try await withUnsafeThrowingContinuation { cont in
+            self.whenComplete { result in
+                switch result {
+                case .success(let value):
+                    cont.resume(returning: value)
+                case .failure(let error):
+                    cont.resume(throwing: error)
+                }
+            }
+        }
+    }
+}
+
+public extension HTTPOperationsClient {
+    
+    /**
+     Submits a request that will not return a response body to this client asynchronously.
+     
+     - Parameters:
+        - endpointPath: The endpoint path for this request.
+        - httpMethod: The http method to use for this request.
+        - input: the input body data to send with this request.
+        - completion: Completion handler called with an error if one occurs or nil otherwise.
+        - asyncResponseInvocationStrategy: The invocation strategy for the response from this request.
+        - invocationContext: context to use for this invocation.
+     - Throws: If an error occurred during the request.
+     */
+    func executeWithoutOutput<InputType,
+            InvocationReportingType: HTTPClientInvocationReporting, HandlerDelegateType: HTTPClientInvocationDelegate>(
+        endpointOverride: URL? = nil,
+        endpointPath: String,
+        httpMethod: HTTPMethod,
+        input: InputType,
+        invocationContext: HTTPClientInvocationContext<InvocationReportingType, HandlerDelegateType>) async throws
+    where InputType: HTTPRequestInputProtocol {
+        return try await executeAsEventLoopFutureWithoutOutput(
+            endpointOverride: endpointOverride,
+            endpointPath: endpointPath,
+            httpMethod: httpMethod,
+            input: input,
+            invocationContext: invocationContext).get()
+    }
+}
+
+#endif
