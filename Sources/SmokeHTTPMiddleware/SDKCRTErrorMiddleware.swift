a7436847a63880e0471025ec800607a04b443cf0
Simon Pilkington <simonpi@amazon.com>
2023-02-22 12:08:48 +1100

    Add SDKCRTErrorMiddleware.

---
+++ Sources/SmokeHTTPMiddleware/SDKCRTErrorMiddleware.swift
@@ -0,0 +1,49 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License").
+// You may not use this file except in compliance with the License.
+// A copy of the License is located at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// or in the "license" file accompanying this file. This file is distributed
+// on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+// express or implied. See the License for the specific language governing
+// permissions and limitations under the License.
+//
+//  SDKCRTErrorMiddleware.swift
+//  SmokeHTTPMiddleware
+//
+
+import SwiftMiddleware
+import ClientRuntime
+import AwsCommonRuntimeKit
+
+public struct SDKCRTErrorMiddleware<Context, ErrorType>: MiddlewareProtocol {
+    public typealias Input = SmokeSdkHttpRequestBuilder
+    public typealias Output = HttpResponse
+    
+    let hostName: String
+    
+    public init(hostName: String) {
+        self.hostName = hostName
+    }
+    
+    public func handle(_ input: SmokeSdkHttpRequestBuilder, context: Context,
+                       next: (SmokeSdkHttpRequestBuilder, Context) async throws -> HttpResponse) async throws
+    -> HttpResponse {
+        do {
+            return try await next(input, context)
+        // if a `CommonRunTimeError` is thrown
+        } catch let error as CommonRunTimeError {
+            // wrap it appropriately
+            throw SdkError<ErrorType>.client(.crtError(error))
+        }
+    }
+}
+
+
+
+
+
+// CommonRunTimeError
